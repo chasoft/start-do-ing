d5c4e15cc143fe77b5aa01bee05233e6324cd6d8
Brian Cao <chasoft@users.noreply.github.com>
2024-02-27 00:01:33 +0700

    demo tab layout for block content

--- app/routes/random.number/content.tsx
+++ app/routes/random.number/content.tsx
@@ -1,11 +1,12 @@
 /* FRAMEWORK */
+import React from "react"
 
 /* THIRD-PARTY PACKAGES */
-import { Button, NumberInput } from "@mantine/core"
+import { Button, NumberInput, Tabs } from "@mantine/core"
 
 /* COMPONENTS & UTILS */
 import { ContentWrapper } from "~/components"
-import { useHelpContents, useUrlSharingData } from "~/utils"
+import { useHelpContents, useTabsSearchParams, useUrlSharingData } from "~/utils"
 
 /* TRANSLATIONS IMPORT */
 
@@ -29,18 +30,76 @@ function RandomNumberForm() {
 	)
 }
 
-export function RandomNumberContent({ className }: { className?: string }) {
+type TabKey = "first-tab" | "second-tab" | "third-tab"
+
+const tabKeys: TabKey[] = ["first-tab", "second-tab", "third-tab"]
+const defaultKey: TabKey = "first-tab"
+
+const tabs: { key: TabKey; label: string }[] = [
+	{
+		key: "first-tab",
+		label: "First tab"
+	},
+	{
+		key: "second-tab",
+		label: "Second tab"
+	},
+	{
+		key: "third-tab",
+		label: "Third tab"
+	}
+]
+
+const tabContents: { key: TabKey; content: React.ReactNode }[] = [
+	{
+		key: "first-tab",
+		content: <div>First Tab Content</div>
+	},
+	{
+		key: "second-tab",
+		content: <RandomNumberForm />
+	},
+	{
+		key: "third-tab",
+		content: <div>3 third Content</div>
+	}
+]
+
+export function RandomNumberContent() {
+	const [selectedTabKey, onTabChange] = useTabsSearchParams<TabKey>({
+		keys: tabKeys,
+		defaultKey
+	})
 	const urlSharingData = useUrlSharingData(RANDOM_NUMBER)
 	useHelpContents(helpContents)
 	return (
 		<ContentWrapper urlSharingData={urlSharingData} helpContents={helpContents}>
-			<div className={className}>
-				<div className="h-full p-2 sm:p-4 xl:p-6">
-					<h1 className="text-lg font-semibold sm:text-2xl">Random Number</h1>
+			<Tabs value={selectedTabKey} onChange={onTabChange}>
+				<Tabs.List>
+					<h1 className="px-3 py-2 text-lg font-semibold sm:text-2xl">
+						{RANDOM_NUMBER.title}
+					</h1>
+					{tabs.map((tab) => (
+						<Tabs.Tab
+							key={tab.key}
+							value={tab.key}
+							className={tab.key === selectedTabKey ? "font-bold" : ""}
+						>
+							{tab.label}
+						</Tabs.Tab>
+					))}
+				</Tabs.List>
 
-					<RandomNumberForm />
-				</div>
-			</div>
+				{tabContents.map((tabContent) => (
+					<Tabs.Panel
+						key={tabContent.key}
+						value={tabContent.key}
+						className="p-2 sm:p-4 xl:p-6"
+					>
+						{tabContent.content}
+					</Tabs.Panel>
+				))}
+			</Tabs>
 		</ContentWrapper>
 	)
 }
