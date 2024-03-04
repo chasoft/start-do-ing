f4ed2ba98af4ce4949ec79356db8e3d6871307a3
Brian Cao <chasoft@gmail.com>
2024-03-04 02:23:31 +0000

    refactor: TextResize component

--- app/components/TextResize.tsx
+++ app/components/TextResize.tsx
@@ -1,40 +1,42 @@
-import React from "react"
+import { useEffect, useRef } from "react"
 
 type TextResizeProps = {
 	children: React.ReactNode
 }
-export default function TextResize({ children }: TextResizeProps) {
-	const resizeLayer = React.useRef<HTMLDivElement>(null)
 
-	React.useEffect(() => {
+const dynamicFontsizeVar = "--dynamic-font-size"
+const parentPaddingTopVar = "--resize-layer-padding-top"
+const standardFontSize = "1em"
+const parentLayerClasses =
+	"h-fit w-fit mt-[var(--resize-layer-padding-top)] text-[length:var(--dynamic-font-size)]"
+
+export default function TextResize({ children }: TextResizeProps) {
+	const resizeLayer = useRef<HTMLDivElement>(null)
+	useEffect(() => {
 		const targetEl = resizeLayer.current ?? document.body
 		const elObserver = new ResizeObserver((entries) => {
+			const targetEl = entries[0].target as HTMLDivElement
+
 			// Apply for the very first entry in the list only
-			;(entries[0].target as HTMLDivElement).style.setProperty(
-				"--random-word-font-size",
-				"1em"
-			)
+			targetEl.style.setProperty(dynamicFontsizeVar, standardFontSize)
 
 			const { width: max_width, height: max_height } =
-				entries[0].target.parentElement?.getBoundingClientRect() ?? {
-					width: 0,
-					height: 0
-				}
+				targetEl.parentElement?.getBoundingClientRect() ?? { width: 0, height: 0 }
 
-			const { width, height } = entries[0].target.getBoundingClientRect()
+			const { width, height } = targetEl.getBoundingClientRect()
 
 			const sizeRatio = Math.min(max_width / width, max_height / height)
 
-			;(entries[0].target as HTMLDivElement).style.setProperty(
-				"--random-word-font-size",
-				sizeRatio + "em"
-			)
-			;(entries[0].target as HTMLDivElement).style.setProperty(
-				"--resize-layer-padding-top",
+			targetEl.style.setProperty(dynamicFontsizeVar, sizeRatio + "em")
+
+			targetEl.style.setProperty(
+				parentPaddingTopVar,
 				Math.floor((max_height - height * sizeRatio) / 4) + "px"
 			)
 		})
+
 		elObserver.observe(targetEl)
+
 		return () => {
 			elObserver.unobserve(targetEl)
 		}
@@ -42,11 +44,7 @@ export default function TextResize({ children }: TextResizeProps) {
 
 	return (
 		<div className="flex h-full w-full max-w-7xl justify-center">
-			<div
-				id="resizeLayer"
-				ref={resizeLayer}
-				className="mt-[var(--resize-layer-padding-top)] h-fit w-fit text-[length:var(--random-word-font-size)]"
-			>
+			<div id="resizeLayer" ref={resizeLayer} className={parentLayerClasses}>
 				{children}
 			</div>
 		</div>
