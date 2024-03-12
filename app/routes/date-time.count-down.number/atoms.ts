55b2c3888723c46862075ee4c871617691af39d7
Brian Cao <chasoft@gmail.com>
2024-03-12 11:05:31 +0000

    Update count down number to v1.1.0 - add ending name & message - add simple animation

--- app/routes/date-time.count-down.number/atoms.ts
+++ app/routes/date-time.count-down.number/atoms.ts
@@ -1,5 +1,10 @@
 import { atomWithStorage } from "jotai/utils"
-import { MarkdownString } from "~/utils/types"
+
+/***************************************************************************
+ *
+ *  START
+ *
+ **************************************************************************/
 
 type CountdownNumber = {
 	startAt: number
@@ -20,48 +25,6 @@ export const countdownNumberAtom = atomWithStorage<CountdownNumber>(
 		loop: false,
 		cycle: 0,
 		name: "",
-		message: "Time is up!"
-	}
-)
-
-type CountdownTime = {
-	tartgetTime: {
-		seconds: number
-		minutes: number
-		hours: number
-	}
-	loop: boolean
-	cycle: number
-	name: string
-	message: string
-}
-
-export const countdownTimeAtom = atomWithStorage<CountdownTime>(
-	"datetime.countdown.time",
-	{
-		tartgetTime: {
-			seconds: 0,
-			minutes: 0,
-			hours: 0
-		},
-		loop: false,
-		cycle: 0,
-		name: "",
-		message: "Time is up!"
-	}
-)
-
-type CountdownDate = {
-	targetDate: Date
-	name: string
-	message: MarkdownString
-}
-
-export const countdownDateAtom = atomWithStorage<CountdownDate>(
-	"datetime.countdown.Date",
-	{
-		targetDate: new Date("08/03/2024"),
-		name: "International Woman Day",
-		message: "Ngày Quốc Tế Phụ Nữ"
+		message: ""
 	}
 )
